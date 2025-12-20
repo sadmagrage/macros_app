cc6a074f01b003df04357f017074ca916f1e75a4
sadmagrage <victorgoyapvgp09@gmail.com>
2025-12-19 23:50:07 -0300

    feat: use auth logic in auth page

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,19 +1,23 @@
 import { AuthContainer, AuthFieldContainer, AuthForm, AuthInputButton, AuthInputText, AuthLabel, AuthTitle } from './styles';
+import { useAuth } from './useAuth';
 
 export default function Auth() {
+
+     const { username, password, setUsername, setPassword, onSend, authenticate } = useAuth();
+     
      return (
           <AuthContainer>
                <AuthForm>
                     <AuthTitle>Sign In</AuthTitle>
                     <AuthFieldContainer>
                          <AuthLabel>Username</AuthLabel>
-                         <AuthInputText type='text' />
+                         <AuthInputText type='text' value={ username } onChange={ e => setUsername(e.target.value) } />
                     </AuthFieldContainer>
                     <AuthFieldContainer>
                          <AuthLabel>Password</AuthLabel>
-                         <AuthInputText type='password' />
+                         <AuthInputText type='password' value={ password } onChange={ e => setPassword(e.target.value) } />
                     </AuthFieldContainer>
-                    <AuthInputButton>Send</AuthInputButton>
+                    <AuthInputButton onClick={ authenticate } >Send</AuthInputButton>
                </AuthForm>
           </AuthContainer>
      )
