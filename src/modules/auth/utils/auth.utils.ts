b72d73f657b5cce71f259d1ba64a3dc50cf5e1d8
sadmagrage <victorgoyapvgp09@gmail.com>
2025-12-27 23:11:59 -0300

    feat: add auth utility file

--- src/modules/auth/utils/auth.utils.ts
+++ src/modules/auth/utils/auth.utils.ts
@@ -1,11 +1,18 @@
-import type AuthResponse from "../dtos/responses/auth.response"
+import type AuthResponse from "../dtos/responses/auth.response";
+import { jwtDecode, type JwtPayload } from "jwt-decode";
 
 const AUTH_TOKEN_KEY: string = "auth_token";
 
-export const storeToken = (authResponse: AuthResponse) => {
-     localStorage.setItem(AUTH_TOKEN_KEY, authResponse.token);
-}
+export const storeToken = (authResponse: AuthResponse) => localStorage.setItem(AUTH_TOKEN_KEY, authResponse.token);
 
-export const getToken = (): string | null => {
-     return localStorage.getItem(AUTH_TOKEN_KEY);
+export const getToken = (): string | null => localStorage.getItem(AUTH_TOKEN_KEY);
+
+export const isTokenExpired = (token: string): boolean => {
+     const dtNow = Date.now() / 1000;
+
+     const jwtInfo = jwtDecode<JwtPayload>(token);
+
+     if (jwtInfo == null || jwtInfo.exp == null) throw new Error();
+
+     return jwtInfo.exp < dtNow;
 }
