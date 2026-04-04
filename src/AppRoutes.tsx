4f17d3a8cbdd1b62f568acee7b48c3c2bbc2470e
sadmagrage <victorgoyapvgp09@gmail.com>
2026-04-04 20:42:16 -0300

    feat: add food page to the router

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
+import Food from "./pages/Food";
 
 export default function AppRoutes() {
      return (
           <Routes>
                <Route element={ <Home/> } path="/home" />
                <Route element={ <Auth/> } path="/auth" />
+               <Route element={ <Food/> } path="/food" />
           </Routes>
      )
 }
